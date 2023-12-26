582e0253ae47b546d0362dea2fcde13fe421b8d9
DESKTOP-NIDT4N1\MarcSanchez <msanchez@hightech-corp.com>
2023-12-26 13:29:06 -0600

    cambio nombres de carpetas

---
+++ 7-Formularios de Flask/app.py
@@ -0,0 +1,39 @@
+# a) Creación de Formularios:
+
+# Flask-WTF es una extensión de Flask que integra WTForms, una biblioteca de formularios de Python. Comencemos con un ejemplo simple de creación de formulario.
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+
+class MiFormulario(FlaskForm):
+    nombre = StringField('Nombre')
+    edad = IntegerField('Edad')
+    enviar = SubmitField('Enviar')
+
+
+# b) Uso en una Ruta de Flask:
+
+# Ahora, en tu ruta de Flask, puedes renderizar el formulario y manejar las solicitudes.
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tu_clave_secreta_aqui'
+
+
+@app.route('/formulario_ejemplo', methods=['GET', 'POST'])
+def formulario_ejemplo():
+    form = MiFormulario()
+
+    if form.validate_on_submit():
+        # Aquí manejas la lógica del formulario
+        nombre = form.nombre.data
+        edad = form.edad.data
+        # ... (puedes almacenar estos datos en la base de datos, por ejemplo)
+        return f'Nombre: {nombre}, Edad: {edad}'
+
+    return render_template('Mostrar_Formulario_de_Flask.html', form=form)
+
+
+if  __name__ == '__main__':
+    app.run(debug=True)
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\marc\Desktop\Progamacion Python\Flask_ChatGPT\env
+set VIRTUAL_ENV=C:\Users\MarcSanchez\Desktop\python\Python_ChatGPT\env
 
 if not defined PROMPT set PROMPT=$P$G
 
